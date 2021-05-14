ae0d20da1126d02cc9413579f44954bdf8c5bc96
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-14 10:35:13 +0530

    Prefix and Suffix search using HashMap in C++ - comments added

--- Week1/day1.cpp
+++ Week1/day1.cpp
@@ -1,10 +1,12 @@
 class WordFilter {
 private:
+
     //creating map for stroing all combinations of prefix and suffix
     unordered_map<string,int> mymap;
     
 public:    
     WordFilter(vector<string>& words) {
+
         //calculating given number of words
         int total = words.size();
        
@@ -34,6 +36,7 @@ public:
         //here we are returning -1 because of default case in which we have return -1 
         // acc to map knowledge , if value is not found in map it returns 0 so doing -1;
         //thats why we stored 1 more than the index prior
+
         return mymap[temp] - 1;
     }
 };
