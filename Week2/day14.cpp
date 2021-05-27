e120e2fde4d038eb196eac83ee4ceebbdce0b8d7
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-27 23:30:44 +0530

    Flatten Binary Tree to Linked List in C++ - comments added

--- Week2/day14.cpp
+++ Week2/day14.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
     void flatten(TreeNode* root) {
         
         //if root is null means no binary tree 
@@ -14,10 +15,12 @@ public:
         
         while(!mystack.empty())
         {
+
             /*
             we add the curr node and store the right element first in stack then the left elemrnt
             because LIFO so we put left at last to add left node before right node
             */
+
             TreeNode* curr = mystack.top();
             mystack.pop();
             
@@ -32,6 +35,7 @@ public:
             //now adding the top of stack that is left node is added to curr node acc to preorder traversal
             if(!mystack.empty())
                 curr->right = mystack.top();
+
             curr->left = NULL;
         }
     }
