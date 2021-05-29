e0acf1d9a272d2035a2ecf825379421a3c9eb96a
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-29 21:26:03 +0530

    Binary Tree Camera's LeetCode hard problem in C++ - comments added

--- Week3/day16.cpp
+++ Week3/day16.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+
     int ncamera;
     int minCameraCover(TreeNode* root) {
         
         //we will be using dfs to go down to leafs and while returning we will assign the camera at best posn
         //hence it is greedy approach as well
+
         ncamera =0;
         pair<bool,bool> temp = count_camera(root);
         if(!temp.second)
@@ -21,6 +23,7 @@ public:
         1. has camera  (if it has camera then it is always monitored)
         2. is monitored , 
         */
+
         pair<bool,bool> l = count_camera(root->left);
         pair<bool,bool> r = count_camera(root->right);
         
@@ -41,5 +44,6 @@ public:
         
         //return the bool pair telling has camera and is monitored or not 
         return {has_cam,monitored};
+
     }
 };
