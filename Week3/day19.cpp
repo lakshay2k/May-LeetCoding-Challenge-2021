15ae62c9c936b484aa80165abf22ed516d5bd78d
lakshay2k <lakshaykmsharma@gmail.com>
2021-06-01 10:13:39 +0530

    Minimum moves to equal Array Elements ll

--- Week3/day19.cpp
+++ Week3/day19.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+
     int minMoves2(vector<int>& nums) {
+
         sort(nums.begin(), nums.end());
+
         int ans = 0, median = nums[~~(nums.size() / 2)];
         for (auto num : nums) ans += abs(median - num);
+
         return ans;
     }
 };
