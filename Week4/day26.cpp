3565c76559b3d897bd44c868223e10039158538f
lakshay2k <lakshaykmsharma@gmail.com>
2021-06-06 11:07:05 +0530

    Partitioning Into Minimum Number Of Deci-Binary Numbers in C++

---
+++ Week4/day26.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        
+        //the minimum number of trun to return a decibinary number is the max at that position
+        char maximum_number = '0';
+        
+        //iteration over every number of given digit
+        for(auto &c : n)
+        {
+            if(c>maximum_number)
+                maximum_number = c;
+        }
+        
+        //returning the max number
+        return maximum_number-'0';
+    }
+};
