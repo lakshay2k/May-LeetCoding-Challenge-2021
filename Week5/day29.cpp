a15c73ae099d079662cfc285587fa1e30c4d1260
lakshay2k <lakshaykmsharma@gmail.com>
2021-06-08 09:45:20 +0530

    N-Queens || in C++

---
+++ Week5/day29.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        vector<vector<string>> vec;
+		// Inititally filling all with '.' as we have to mark Q
+        vector<string> board(n,string(n,'.')); 
+        backtrack(vec,board,0,n);
+        return vec.size();
+    }
+    
+    void backtrack(vector<vector<string>> &vec,vector<string> & board,int row,int &n) 
+    {        
+	    // Base case
+        if(row == n)
+        {
+            vec.push_back(board);
+            return;
+        }
+		// Filling columns one by one
+        for(int col=0;col<n;++col) 
+        {
+            if(isValid(board,row,col,n))
+            {
+                board[row][col] = 'Q';
+				// Recursing row by row
+                backtrack(vec,board,row+1,n); 
+				// Backtracking by removing queen from the cell to explore other choices
+                board[row][col] = '.'; 
+            }
+        }
+    }
+    bool isValid(vector<string> &board,int row,int col,int n)
+    {    
+	    // If column has queen already
+        for(int i=row-1;i>=0;i--) 
+        {
+            if(board[i][col] == 'Q')
+                return false;
+        } 
+	    // Diagonal
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--)
+        {
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        // Antidiagonal
+        for(int i=row-1,j=col+1;i>=0 && j<board.size();i--,j++)
+        {
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        
+		// We are not checking for rows as we are filling them one after the other
+        return true;
+    }
+};
